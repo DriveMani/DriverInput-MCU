216096ca3ecf3a5ddd91274f96cf25850a263271
CBAAabced <nmrkmj0108@kyonggi.ac.kr>
2026-03-20 20:57:48 +0900

    feat: 통합 (input_diag 오류)

---
+++ integreated_driverinput/Configuration/System/DBImport/Project_version.h
@@ -0,0 +1,11 @@
+/*
+* Date: 2026. 03. 19.
+* Path: C:/realwk/autoever_autosar_edu_Warrior_mpc5606b_Base_R231101/References/DB/Project.dbc
+* * [Invalid Version Number] Version Number should be number.* Project_VERSION : Empty*/
+#define Project_VERSION 
+#define Project_VERSIONNUMBERINTERNAL 0
+#define Project_VERSIONUMBER 0
+#define Project_VERSIONWEEK 1
+#define Project_VERSIONYEAR 0
+#define Project_VERSIONMONTH 1
+#define Project_VERSIONDAY 1
